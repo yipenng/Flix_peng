cbd4cdf63d9c8bc6c9124506ef670f50196b4599
Yipeng <Yipengg@gmail.com>
2021-09-05 21:54:52 -0400

    Initial Commit

---
+++ Flix_peng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix_peng
+//
+//  Created by yipeng li on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flix_pengTests/Flix_pengTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flix_pengTests.swift
+//  Flix_pengTests
+//
+//  Created by yipeng li on 9/5/21.
+//
+
+import XCTest
+@testable import Flix_peng
+
+class Flix_pengTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
